0bb5c335c3bbac2c6e2d7740bd77de8ccd16f17d
LAPTOP-FELIX\Felix <r0628578@student.ucll.be>
2018-12-10 15:23:39 +0100

    fixed nummer

--- src/model/Adres.java
+++ src/model/Adres.java
@@ -1,17 +1,20 @@
 package model;
 
 import exception.ModelException;
+
+import javax.jws.WebParam;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Adres {
 
-    private String straat, plaats, huisnummer;
-    private int bus;
+    private String straat, plaats, nummer;
+    private int bus, postcode;
 
-    public Adres(String straat, String plaats, String huisnummer, int bus) throws ModelException {
+    public Adres(String straat, String plaats, String nummer, int bus, int postcode) throws ModelException {
+        setPostcode(postcode);
         setStraat(straat);
-        setHuisnummer(huisnummer);
+        setNummer(nummer);
         setBus(bus);
         setPlaats(plaats);
     }
@@ -20,23 +23,26 @@ public class Adres {
         return bus;
     }
 
-    public void setBus(int bus) {
+    public void setBus(int bus) throws ModelException {
         this.bus = bus;
+        if(bus <= 0){
+            throw new ModelException("Gelieve een geldige bus in te geven");
+        }
     }
 
-    public String getHuisnummer() {
-        return huisnummer;
+    public String getNummer() {
+        return nummer;
     }
 
-    public void setHuisnummer(String huisnummer) throws ModelException {
-        this.huisnummer = huisnummer;
+    public void setNummer(String nummer) throws ModelException {
+        this.nummer = nummer;
         //TODO kijk na of de regex werkt.
-        if(huisnummer.isEmpty() || huisnummer == null){
-            throw new ModelException("Huisnummer kan niet leeg zijn");
+        if(nummer.isEmpty() || nummer == null){
+            throw new ModelException("nummer kan niet leeg zijn");
         }
         String HUISNUMMER_PATTERN = "^(.\\d{3}[a-zA-Z])$";
         Pattern p = Pattern.compile(HUISNUMMER_PATTERN);
-        Matcher m = p.matcher(huisnummer);
+        Matcher m = p.matcher(nummer);
         if(!m.matches()){
             throw new ModelException("Huisnummer not valid");
         }
@@ -61,6 +67,17 @@ public class Adres {
         this.straat = straat;
         if(straat.trim().isEmpty() || straat == null){
             throw new ModelException("Straat kan niet leeg zijn");
+        }
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) throws ModelException {
+        this.postcode = postcode;
+        if(postcode <= 0 || postcode > 9999){
+            throw new ModelException("Gelieve een geldige postcode in te geven");
         }
     }
 }
